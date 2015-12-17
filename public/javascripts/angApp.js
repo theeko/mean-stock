e5524138e5d009a6beef553b35b9c33856ada0e8
kairath <thekairath@hotmail.com>
2015-12-17 00:31:21 +0000

    works

--- public/javascripts/angApp.js
+++ public/javascripts/angApp.js
@@ -4,8 +4,18 @@
   var app = angular.module("myApp", ["chart.js"]);
   
   app.controller("mainCtrl", ["$scope", "stocks", "$http", function ($scope, stocks,$http) {
-    $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-                     'August', 'September', 'October', 'November', 'December'];
+    $scope.labelsRef = ['Jan', 'Feb', 'March', 'Apr', 'May', 'June', 'July',
+                     'Aug', 'Sept', 'Octo', 'Nov', 'Dec'];
+    $scope.labels = [];
+     var date = new Date();
+      var dday = date.getDate();
+      var dmonth = date.getMonth() -1;
+      for(var x=0; x<30; x++){
+        if(dday > 30){ dday = 0; dmonth +=1;}
+        dday += 1;
+        $scope.labels.push(dday.toString() + "-" + $scope.labelsRef[dmonth]);
+    }
+    
       $scope.series = stocks.datas.series;
       // $scope.data = [
       //   [65, 59, 80, 81, 56, 55, 40],
@@ -18,13 +28,18 @@
     $scope.findData = function(input){
       var x = x = new Date();
       var xMotnh = x.getMonth() < 10 ? "0" +x.getMonth() : x.getMonth();
-      var y = "0"+(x.getMonth() - 6);
+      var y = x.getMonth() - 1 < 10 ? "0" + (x.getMonth() -1) : x.getMonth() -1;
       var xDay = x.getDay() < 10 ? "0" + x.getDay() : xDay;
       var date1 = x.getFullYear() + "-" + xMotnh + "-" + xDay;
       var date2 = x.getFullYear() + "-" + y + "-" + xDay;
       var url = "https://www.quandl.com/api/v3/datasets/WIKI/" + input + ".json?api_key=PKtdZ-7LH8AuwUhHdozR&start_date=" + date2 + "&end_date=" + date1;
       stocks.getStock(url, input);
       $scope.input = "";
+    };
+    
+    $scope.delete = function (ind) {
+      stocks.datas.series.splice(ind,1);
+      stocks.datas.data.splice(ind,1);
     };
     
   }]);
